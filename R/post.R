ff061ba968d63306700ab67c747eba66cce63153
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-06-17 19:33:47 +0200

    fix #4

--- R/post.R
+++ R/post.R
@@ -17,7 +17,7 @@
 wp_post <- function(post_folder, wordpress_url) {
 
    body <- commonmark::markdown_html(
-     glue::glue_collapse(blogdown:::split_yaml_body(readLines(path))$body,
+     glue::glue_collapse(split_yaml_body(readLines(path))$body,
                          sep = "\n"),
      extensions = TRUE)
 
@@ -57,5 +57,35 @@ wp_post <- function(post_folder, wordpress_url) {
 }
 
 
+# from https://github.com/rstudio/blogdown/blob/9ce8c0d0d8e3cadfa26b216e2a58d2e313f9b6bd/R/utils.R#L442
+# split Markdown to YAML and body (adapted from xaringan)
+split_yaml_body = function(x) {
+  i = grep('^---\\s*$', x)
+  n = length(x)
+  res = if (n < 2 || length(i) < 2 || (i[1] > 1 && !knitr:::is_blank(x[seq(i[1] - 1)]))) {
+    list(yaml = character(), body = x)
+  } else list(
+    yaml = x[i[1]:i[2]], yaml_range = i[1:2],
+    body = if (i[2] == n) character() else x[(i[2] + 1):n]
+  )
+  res$yaml_list = if ((n <- length(res$yaml)) >= 3) {
+    yaml_load(res$yaml[-c(1, n)])
+  }
+  res
+}
 
+# anotate seq type values because both single value and list values are
+# converted to vector by default
+yaml_load = function(x) yaml::yaml.load(
+  x, handlers = list(
+    seq = function(x) {
+      # continue coerce into vector because many places of code already assume this
+      if (length(x) > 0) {
+        x = unlist(x, recursive = FALSE)
+        attr(x, 'yml_type') = 'seq'
+      }
+      x
+    }
+  )
+)
 
