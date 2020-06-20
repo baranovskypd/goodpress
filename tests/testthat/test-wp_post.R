184fa201128d87b295a36f3caf5b6807b7cca936
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-06-20 08:37:45 +0200

    organization

--- tests/testthat/test-wp_post.R
+++ tests/testthat/test-wp_post.R
@@ -1,19 +1,22 @@
 test_that("wp_post works", {
-  # create file under tests for that (with a .wordpress.yml file!)
   vcr::use_cassette("example2", {
 
+    # Copy content of example post folder to a temporary folder
     post_folder <- system.file(file.path("post-example2"), package = "goodpress")
     tmp_post_folder <- file.path(tempdir(), "post")
     dir.create(tmp_post_folder)
     file.copy(dir(post_folder, full.names = TRUE), tmp_post_folder)
-    file.copy(test_path("wordpress.yml"), file.path(tmp_post_folder, ".wordpress.yml"))
-    wordpress_url <- "https://rmd-wordpress.eu"
 
+    # add the hidden wordpress YAML file
+    file.copy(test_path("wordpress.yml"), file.path(tmp_post_folder, ".wordpress.yml"))
+
+    # Just test that... we don't get errors :-s
     x <- wp_post(tmp_post_folder,
                  wordpress_url = "https://rmd-wordpress.eu")
 
     expect_is(x, "character")
 
+    # Delete the temporary folder
     file.remove(dir(tmp_post_folder, full.names = TRUE))
 
   })
